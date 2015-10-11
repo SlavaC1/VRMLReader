43a3d1ec4e89317b8cff8d4092f8cb544944a2d3
SlavaC1 <wastelanders@gmail.com>
2015-10-11 23:44:29 +0300

    - Making grammars run faster and correctly - Combining 2 grammars

--- VRMLReader/VRMLReader/VRMLParser.cpp
+++ VRMLReader/VRMLReader/VRMLParser.cpp
@@ -55,41 +55,72 @@ BOOST_FUSION_ADAPT_STRUCT
 namespace qi   = boost::spirit::qi;
 namespace repo = boost::spirit::repository;
 
+struct VRML_t
+{
+	std::vector<Point>      points;
+	std::vector<CoordIndex> indexes;
+};
+
+BOOST_FUSION_ADAPT_STRUCT
+(
+	VRML_t,
+	(std::vector<Point>,      points)
+	(std::vector<CoordIndex>, indexes)
+)
+
 
 template <typename Iterator>
-struct PointParser : boost::spirit::qi::grammar<Iterator, std::vector<Point>(), qi::space_type>
+struct PointParser : qi::grammar<Iterator, std::vector<Point>(), qi::space_type>
 {
 	PointParser() : PointParser::base_type(start, "PointGrammar")
 	{
 		singlePoint = qi::double_ >> qi::double_ >> qi::double_ >> *qi::lit(",");
-		comment     = qi::lit("#") >> *(qi::char_ - qi::eol);
-		prefix      = repo::seek[qi::lexeme[qi::skip[qi::lit("point") >> qi::lit("[") >> *comment]]];
-		start      %= prefix >> qi::repeat[singlePoint];		
+		comment     = "#" >> *(qi::ascii::char_ - qi::eol);
+		prefix      = repo::seek[qi::lit("point") >> "[" >> *comment];
+		start      %= prefix >> *singlePoint;		
 
 		//BOOST_SPIRIT_DEBUG_NODES((prefix)(comment)(singlePoint)(start));
 	}
 
 	qi::rule<Iterator, Point(), qi::space_type>              singlePoint;
-	qi::rule<Iterator, qi::space_type>                       comment;
+	qi::rule<Iterator>                                       comment;
 	qi::rule<Iterator, qi::space_type>                       prefix;
 	qi::rule<Iterator, std::vector<Point>(), qi::space_type> start;
 };
 
 template <typename Iterator>
-struct CoordIndexParser : boost::spirit::qi::grammar<Iterator, std::vector<CoordIndex>(), qi::space_type>
+struct CoordIndexParser : qi::grammar<Iterator, std::vector<CoordIndex>(), qi::space_type>
 {
 	CoordIndexParser() : CoordIndexParser::base_type(start, "CoordIndexGrammar")
 	{
 		singleIndex  = qi::int_ >> qi::int_ >> qi::int_ >> qi::lit("-1") >> *qi::lit(",");
-		start       %= repo::seek[qi::lexeme[qi::skip[qi::lit("coordIndex") >> qi::lit("[") >> qi::repeat[singleIndex]]]];		
+		comment      = "#" >> *(qi::ascii::char_ - qi::eol);
+		prefix       = repo::seek[qi::lit("coordIndex") >> "[" >> *comment];
+		start       %= prefix >> *singleIndex;		
 
-		//BOOST_SPIRIT_DEBUG_NODES((singleIndex)(start));
+		//BOOST_SPIRIT_DEBUG_NODES((start)(prefix)(singleIndex));
 	}
 
 	qi::rule<Iterator, CoordIndex(), qi::space_type>              singleIndex;
+	qi::rule<Iterator>                                            comment;
+	qi::rule<Iterator, qi::space_type>                            prefix;
 	qi::rule<Iterator, std::vector<CoordIndex>(), qi::space_type> start;	
 };
 
+
+template <typename Iterator>
+struct VRMLParser : qi::grammar<Iterator, VRML_t(), qi::space_type>
+{
+	VRMLParser() : VRMLParser::base_type(start, "VRMLGrammar")
+	{
+		start %= pointParser >> coordIndexParser;
+	}
+
+	PointParser<Iterator>      pointParser;
+	CoordIndexParser<Iterator> coordIndexParser;
+	qi::rule<Iterator, VRML_t(), qi::space_type> start;
+	
+};
 
 
 
@@ -106,16 +137,24 @@ void CVRMLParser::Parse(const std::string &Data)
 
 
 	std::vector<Point> points;
-	typedef PointParser<std::string::const_iterator> pointParser;	
+	typedef PointParser<std::string::const_iterator> pointParser;
 	pointParser g2;
 
+	VRML_t VRML;
+	typedef VRMLParser<std::string::const_iterator> finalParser;
+	finalParser g3;
+
 	auto start = ch::high_resolution_clock::now();	
-	bool r = phrase_parse(Data.begin(), Data.end(), g2, qi::space, points);	
+	bool r = phrase_parse(Data.begin(), Data.end(), g3, qi::space, VRML);	
 	auto end = ch::high_resolution_clock::now();
 
 	auto duration = ch::duration_cast<boost::chrono::milliseconds>(end - start).count();
 
-	int i = 2;
+	std::cout << "The duration is: " << duration << " msec" << std::endl;
+	
+	std::cout << "Parsed indexes: " << VRML.indexes.size() << std::endl << "Parsed points: " << VRML.points.size() << std::endl;
+
+	
 
 	
 	/*
