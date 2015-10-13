30121153848738c13060acec8b7ecb7e9a6d90be
SlavaC1 <wastelanders@gmail.com>
2015-10-13 23:49:05 +0300

    - Adding closing brackets

--- VRMLReader/VRMLReader/Grammar.cpp
+++ VRMLReader/VRMLReader/Grammar.cpp
@@ -12,7 +12,7 @@ namespace VRMLDoc
 		m_SinglePointRule = double_ >> double_ >> double_ >> *lit(",");
 		m_PrefixRule      = seek[lit("point") >> "[" >> *m_CommentRule];
 		m_CommentRule     = "#" >> *(ascii::char_ - eol);
-		m_StartRule      %= m_PrefixRule >> *m_SinglePointRule;
+		m_StartRule      %= m_PrefixRule >> *m_SinglePointRule >> "]";
 
 		//BOOST_SPIRIT_DEBUG_NODES((m_PrefixRule)(m_CommentRule)(m_SinglePointRule)(m_StartRule));
 	}
@@ -22,7 +22,7 @@ namespace VRMLDoc
 		m_SingleIndexRule = int_ >> *lit(",") >> int_ >> *lit(",") >> int_ >> *lit(",") >> lit("-1") >> *lit(",");
 		m_PrefixRule      = seek[lit("coordIndex") >> "[" >> *m_CommentRule];
 		m_CommentRule     = "#" >> *(ascii::char_ - eol);
-		m_StartRule      %= m_PrefixRule >> *m_SingleIndexRule;
+		m_StartRule      %= m_PrefixRule >> *m_SingleIndexRule >> "]";
 
 		//BOOST_SPIRIT_DEBUG_NODES((m_StartRule)(m_PrefixRule)(singleIndexm_SingleIndexRule));
 	}
